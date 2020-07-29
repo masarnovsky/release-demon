6818f0d4d486a86825f0c7ac4ace06fe82520961
m.masarnovsky <m.masarnovsky@gmail.com>
2020-07-29 14:36:45 +0300

    init commit

---
+++ src/main/java/by/masarnovsky/releasedemon/ReleaseDemonApplication.java
@@ -0,0 +1,12 @@
+package by.masarnovsky.releasedemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseDemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleaseDemonApplication.class, args);
+	}
+}
