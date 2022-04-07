c4d6cf199ccb16e8fc49583c1acac732952c3d07
m.masarnovsky <m.masarnovsky@gmail.com>
2022-04-07 14:34:14 +0300

    update docker-compose file with correct image for app. update bash script with info about current step

--- redeploy.sh
+++ redeploy.sh
@@ -2,12 +2,21 @@
 
 # chmod u+x redeploy.sh to make file executable
 
+echo "stopping release-demon container"
 docker stop release-demon
+echo "release-demon container was stopped"
 
+echo "removing release-demon container"
 docker rm release-demon
+echo "release-demon container was removed"
 
+echo "removing release-demon image"
 docker image rm release-demon
+echo "release-demon image was removed"
 
+echo "building new release-demon image"
 docker build -t release-demon .
+echo "release-demon image was build"
 
+echo "running release-demon and mysql from docker-compose"
 docker-compose up -d
